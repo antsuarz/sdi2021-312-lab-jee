cf14afff413590e3e5b8edf6dc3062ce8c3e04d0
antsuarz <uo270543@uniovi.es>
2021-03-04 12:04:57 +0100

    SDI-2021-312-4.2-Roles.

--- sdi2021-312-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi2021-312-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
+import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.SignUpFormValidator;
@@ -24,6 +25,9 @@ public class UsersController {
 	@Autowired
 	private SignUpFormValidator signUpFormValidator;
 
+	@Autowired
+	private RolesService rolesService;
+
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
 		model.addAttribute("usersList", usersService.getUsers());
@@ -32,7 +36,7 @@ public class UsersController {
 
 	@RequestMapping(value = "/user/add")
 	public String getUser(Model model) {
-		model.addAttribute("usersList", usersService.getUsers());
+		model.addAttribute("rolesList", rolesService.getRoles());
 		return "user/add";
 	}
 
@@ -79,9 +83,10 @@ public class UsersController {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(@Validated User user, BindingResult result) {
-		signUpFormValidator.validate(user,result);
-		if(result.hasErrors())
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors())
 			return "signup";
+		user.setRole(rolesService.getRoles()[0]);
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
